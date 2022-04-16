8536edda768a41a2bb46dcb42f64dc25c64e4602
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-04-16 17:42:09 +0900

    시큐리티 Config패키지 생성

---
+++ src/main/java/com/example/securitystudy/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.securitystudy.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+@EnableGlobalMethodSecurity(securedEnabled = true) // @Secured 어노테이션 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors();
+    }
+}
